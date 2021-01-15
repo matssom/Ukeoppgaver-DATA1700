69303d9b5ae13a128bb0154a3fe3b3ec28d4ba33
Mats Sommervold <mats.sommervold@gmail.com>
2021-01-15 12:50:03 +0100

    Fullført oppgave 1

---
+++ src/main/java/com/example/demo/Ukeoppgaver1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ukeoppgaver1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ukeoppgaver1Application.class, args);
+    }
+
+}
