758d1b2c8a395a092d5b7f267af848cca92257ed
Mats Sommervold <mats@sommervold.net>
2021-04-09 11:10:35 +0200

    La til error fallback

---
+++ src/main/java/com/example/demo/ErrorHandler.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import com.example.demo.klientTjener.model.Valuta;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ErrorHandler implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+
+}
